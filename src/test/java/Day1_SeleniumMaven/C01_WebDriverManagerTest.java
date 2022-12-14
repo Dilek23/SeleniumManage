381c16de258bb17a6684df960bd769655e67f330
Tugba Dilek <trydilek23@gmail.com>
2022-12-14 23:49:16 +0300

    first commit

---
+++ src/test/java/Day1_SeleniumMaven/C01_WebDriverManagerTest.java
@@ -0,0 +1,39 @@
+package Day1_SeleniumMaven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_WebDriverManagerTest {
+    public static void main(String[] args) {
+        // Set Path.
+// Create chrome driver.
+// Maximize the window.
+// Open google home page https://www.google.com/
+// Verify that you are Google in the title.
+           //System... yerineEbDriver clasiini kullanarak chromedriver binary indirdik
+        WebDriverManager.chromedriver().setup();
+
+        // Driver objemizi oluşturduk
+        WebDriver driver=new ChromeDriver();
+
+        // window u maximize ettik
+        driver.manage().window().maximize();
+
+        driver.get(" https://www.google.com/");
+        //Titlemizi aldk
+        String title= driver.getTitle();
+
+        //title mizi dogruladık
+        if (title.equalsIgnoreCase("Google")){
+            System.out.println("TEst PASSEd");
+        }else System.out.println("test FAİLED");
+
+          driver.quit();
+
+
+
+
+
+    }
+}
---
+++ src/test/java/Day1_SeleniumMaven/C02_JunitAnnotations.java
@@ -0,0 +1,68 @@
+package Day1_SeleniumMaven;
+
+import org.junit.*;
+
+public class C02_JunitAnnotations {
+    public static void main(String[] args) {
+        /*
+        Test1->
+        Test2->
+        Test3->
+        Test4->geliştirme asamasıında bu yundeb rapora dahil olmasın
+        her testimizden önce ve sonra calıuşması gereken kod blogumuz mevcut
+        tum testlerin öncesinde ve sonrasında calışması gereken methodlarımıız mevcut
+//        1. @Test -> Marks a method as a TEST CASE.
+//        2. @Before : Runs before EACH @Test annotation.
+//        3. @After : Runs after EACH @Test annotation.
+//        4. @BeforeClass : Runs before each class only once.
+//        5. @AfterClass : Runs after each class only once.
+//        6. @Ignore : Skipping a test case.
+
+
+         */
+    }
+        @Test
+        public void test01() {
+            System.out.println("1.test yapiliyor...");
+
+
+
+        }
+    @Test
+    public void test02() {
+        System.out.println("2.test yapiliyor...");
+
+
+    }
+    @Test
+    public void test03() {
+
+        System.out.println("3.test yapiliyor...");
+
+    }
+    @Test
+    @Ignore
+        public void test04() {
+
+
+            System.out.println("4.test gelistirme asamasında...");
+        }
+        @Before
+      public void beforeEach(){
+          System.out.println("Method öncesi kod blogu calıştı...");
+      }
+      @After
+
+    public void afterEach(){
+        System.out.println("Method sonrası kod blogu calıştı...");
+    }
+    @BeforeClass
+    public  static void beforeAll(){
+        System.out.println("Method öncesi kod blogu calıştı...");
+    }
+    @AfterClass
+    public static void afterAll(){
+        System.out.println("Method sonrası kod blogu calıştı...");
+    }
+
+}
